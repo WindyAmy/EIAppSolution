50d31fc31e69e5e0969319920045fbf5439214ed
Windy <515632356@qq.com>
2017-02-10 16:14:52 +0800

    add

---
+++ EIApp.Common/Model2Table.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace EIApp.Common
+{
+    public class Model2Table<T> where T : new()
+    {
+        #region 实体类转换成DataTable
+
+        /// <summary>
+        /// 实体类转换成DataSet
+        /// </summary>
+        /// <param name="modelList">实体类列表</param>
+        /// <returns></returns>
+        public static DataSet FillDataSet(List<T> modelList)
+        {
+            if (modelList == null || modelList.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                DataSet ds = new DataSet();
+                ds.Tables.Add(FillDataTable(modelList));
+                return ds;
+            }
+        }
+
+        /// <summary>
+        /// 实体类转换成DataTable
+        /// </summary>
+        /// <param name="modelList">实体类列表</param>
+        /// <returns></returns>
+        public static DataTable FillDataTable(List<T> modelList)
+        {
+            if (modelList == null || modelList.Count == 0)
+            {
+                return null;
+            }
+            DataTable dt = CreateData(modelList[0]);
+
+            foreach (T model in modelList)
+            {
+                DataRow dataRow = dt.NewRow();
+                foreach (PropertyInfo propertyInfo in typeof(T).GetProperties())
+                {
+                    dataRow[propertyInfo.Name] = propertyInfo.GetValue(model, null);
+                }
+                dt.Rows.Add(dataRow);
+            }
+            return dt;
+        }
+
+        /// <summary>
+        /// 根据实体类得到表结构
+        /// </summary>
+        /// <param name="model">实体类</param>
+        /// <returns></returns>
+        private static DataTable CreateData(T model)
+        {
+            DataTable dataTable = new DataTable(typeof(T).Name);
+            foreach (PropertyInfo propertyInfo in typeof(T).GetProperties())
+            {
+                dataTable.Columns.Add(new DataColumn(propertyInfo.Name, propertyInfo.PropertyType));
+            }
+            return dataTable;
+        }
+
+        #endregion
+    }
+}
---
+++ EIApp.Common/XmlDatasetConvert.cs
@@ -0,0 +1,181 @@
+﻿using System.Data;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace EIApp.Common
+{
+    public class XmlDatasetConvert
+    {
+        #region 将xml字符串转换为DataSet
+
+        /// <summary>
+        /// 将xml对象内容字符串转换为DataSet
+        /// </summary>
+        /// <param name="xmlData"></param>
+        /// <returns></returns>
+        public static DataSet XmlString2DataSet(string xmlData)
+        {
+            StringReader stream = null;
+            XmlTextReader reader = null;
+            DataSet xmlDS = null;
+            try
+            {
+                xmlDS = new DataSet();
+                using (stream = new StringReader(xmlData))
+                {
+                    //从stream装载到XmlTextReader
+                    using (reader = new XmlTextReader(stream))
+                    {
+                        xmlDS.ReadXml(reader);
+                    }
+                }
+                return xmlDS;
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (reader != null) reader.Close();
+            }
+        }
+
+        #endregion 将xml字符串转换为DataSet
+
+        #region 将xml文件转换为DataSet
+
+        /// <summary>
+        /// 将xml文件转换为DataSet
+        /// </summary>
+        /// <param name="xmlFile">文件路径</param>
+        /// <returns></returns>
+        public static DataSet XmlFile2DataSet(string xmlFile)
+        {
+            XmlTextReader reader = null;
+            try
+            {
+                DataSet xmlDS = new DataSet();
+
+                //从stream装载到XmlTextReader
+                using (reader = new XmlTextReader(xmlFile))
+                {
+                    xmlDS.ReadXml(reader);
+                }
+                //xmlDS.ReadXml(xmlFile);
+                return xmlDS;
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (reader != null) reader.Close();
+            }
+        }
+
+        #endregion 将xml文件转换为DataSet
+
+        #region 将DataSet转换为xml对象字符串
+
+        /// <summary>
+        ///  将DataSet转换为xml对象字符串
+        /// </summary>
+        /// <param name="xmlDS"></param>
+        /// <returns></returns>
+        public static string DataSet2XmlString(DataSet xmlDS)
+        {
+            MemoryStream stream = null;
+            XmlTextWriter writer = null;
+
+            try
+            {
+                stream = new MemoryStream();
+                //从stream装载到XmlTextReader
+                //writer = new XmlTextWriter(stream, Encoding.Unicode);//Unicode有点问题，可能是字符集不一致
+                writer = new XmlTextWriter(stream, Encoding.Default);
+
+                //用WriteXml方法写入文件.
+                xmlDS.WriteXml(writer);
+                int count = (int)stream.Length;
+                byte[] arr = new byte[count];
+                stream.Seek(0, SeekOrigin.Begin);
+                stream.Read(arr, 0, count);
+
+                //UnicodeEncoding utf = new UnicodeEncoding();
+                //return utf.GetString(arr).Trim();
+                return Encoding.Default.GetString(arr).Trim();
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (writer != null) writer.Close();
+            }
+        }
+
+        #endregion 将DataSet转换为xml对象字符串
+
+        #region 将DataSet转换为xml文件
+
+        /// <summary>
+        /// 将DataSet转换为xml文件
+        /// </summary>
+        /// <param name="xmlDS"></param>
+        /// <param name="xmlFile"></param>
+        public static void DataSet2XmlFile(DataSet xmlDS, string xmlFile)
+        {
+            try
+            {
+                xmlDS.WriteXml(xmlFile);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+
+            #region 复杂实现方法
+
+            //MemoryStream stream = null;
+            //XmlTextWriter writer = null;
+
+            //try
+            //{
+            //    stream = new MemoryStream();
+            //    //从stream装载到XmlTextReader
+            //    //writer = new XmlTextWriter(stream, Encoding.Unicode);
+            //    writer = new XmlTextWriter(stream, Encoding.Default);
+
+            //    //用WriteXml方法写入文件.
+            //    xmlDS.WriteXml(writer);
+            //    int count = (int)stream.Length;
+            //    byte[] arr = new byte[count];
+            //    stream.Seek(0, SeekOrigin.Begin);
+            //    stream.Read(arr, 0, count);
+
+            //    //返回Unicode编码的文本
+            //    //UnicodeEncoding utf = new UnicodeEncoding();
+            //    StreamWriter sw = new StreamWriter(xmlFile);
+            //    sw.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            //    sw.WriteLine(Encoding.Default.GetString(arr).Trim());
+            //    sw.Close();
+            //}
+            //catch (System.Exception ex)
+            //{
+            //    throw ex;
+            //}
+            //finally
+            //{
+            //    if (writer != null) writer.Close();
+            //}
+
+            #endregion 复杂实现方法
+        }
+
+        #endregion 将DataSet转换为xml文件
+    }
+}
